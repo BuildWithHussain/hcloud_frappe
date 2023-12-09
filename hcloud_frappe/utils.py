d826e09362914474d1b53953f0d7ef33ed0c7f33
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-12-09 18:30:15 +0530

    fix: throw if credentials are not found in config

--- hcloud_frappe/utils.py
+++ hcloud_frappe/utils.py
@@ -2,7 +2,15 @@ import frappe
 
 from hcloud import Client
 
+
 # HETZNER_API_KEY
 def get_hetzner_client():
-	api_key = frappe.conf.get("HETZNER_API_KEY")
-	return Client(token=api_key)
+    api_key = frappe.conf.get("HETZNER_API_KEYS")
+    project_id = frappe.conf.get("HETZNER_PROJECT_ID")
+
+    if not (api_key and project_id):
+        frappe.throw(
+            "Hetzner API Key and Project ID are required before creating a client object. Please place HETZNER_API_KEY and HETZNER_PROJECT_ID in site_config.json."
+        )
+
+    return Client(token=api_key)
